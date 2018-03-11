3ead9c84aea52763d92c3c36ded603a2c1b59e8d
Cooper DeLay <accesschatty@Gmail.com>
2018-03-10 18:52:40 -0800

    Reup spring assignments
    I fucked it up last time

---
+++ 03_Card/Deck.cpp
@@ -0,0 +1,31 @@
+//***************************************************************************
+// File name:  Deck.cpp
+// Author:     Cooper DeLay
+// Date:       3/10/18
+// Class:      CS250
+// Assignment: Card
+// Purpose:    Implements constructers and functions of class Deck
+//***************************************************************************
+
+#include "stdafx.h"
+#include "Deck.h"
+
+Deck::Deck ()
+{
+
+}
+
+void Deck::shuffle ()
+{
+
+}
+
+Card Deck::deal ()
+{
+  return Card ();
+}
+
+int Deck::cardsLeft () const
+{
+  return 0;
+}
---
+++ 03_Card/Deck.h
@@ -0,0 +1,28 @@
+//***************************************************************************
+// File name:  Deck.h
+// Author:     Cooper DeLay
+// Date:       3/10/18
+// Class:      CS250
+// Assignment: Card
+// Purpose:    Declares a programmer-defined class card for the info 
+//             of a Deck
+//***************************************************************************
+#pragma once
+
+#include "Card.h"
+#include <iostream>
+
+class Deck
+{
+
+	public:
+		Deck ();
+		void shuffle ();
+		Card deal ();
+		int cardsLeft () const;
+
+	private:
+		static const int MAX_CARDS_IN_DECK = 52;
+		Card macDeckOfCards[MAX_CARDS_IN_DECK];
+		int mTopCardIndex;
+};
